110f64e2f09e25d3f412afc93ae9f8b48c91a67c
Jessica Ferguson <jferguson1903@gmail.com>
2020-12-11 20:14:49 -0600

    past, present, future time class added

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ var hourContainer = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", 
 
 
 function timeblockDisplay() {
-    // Looping through each hourContrainer item and appending a div that includes a save button
+    var luxonCurrentHour = luxon.DateTime.local().toFormat('ha');
+    var currentHourIndex = hourContainer.indexOf(luxonCurrentHour);
+
+    // Looping through each hourContainer item and appending a div that includes a save button
     for (var i = 0; i < hourContainer.length; i++) {
+
+        if (currentHourIndex === i) {
+            hourGhost = 'present';
+        } else if (currentHourIndex < i) {
+            hourGhost = 'past';
+        } else {
+            hourGhost = 'future';
+        }
+
         $(".container").append(
             `<div class="row">
                 <div id="" class="hour col-sm-1 p-2 text-right">${hourContainer[i]}</div>
-                <div id="" class="past present future col-sm-10">
+                <div id="" class="${hourGhost} col-sm-10 px-0">
                     <textarea id="textArea${hourContainer[i]}" class="w-100 h-100"></textarea>
                 </div>
                 <button id="" class="saveBtn col-sm-1" value="${hourContainer[i]}">
@@ -29,6 +41,10 @@ $("#currentDay").append(currentDay);
 // On Click event listener for saveBtn
 $(document.body).on('click', '.saveBtn', function() {
     var saveBtnValue = $(this).val();
-    var userChoice = $(`#textArea${saveBtnValue}`).val();
-    console.log(userChoice);
+    var userEntry = $(`#textArea${saveBtnValue}`).val();
+    localStorage.setItem(`DP_${luxon.DateTime.local().toFormat('LL-dd-yyyy')}_${saveBtnValue}`, userEntry);
 });
+
+function calendarPageLoad() {
+    localStorage.getItem(saveBtnValue, userEntry);
+}
